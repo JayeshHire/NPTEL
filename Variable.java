1d4af0d9dae7cc7144c4b1fd5553eedacbd6b38e
Jayesh <jayeshhire96@gmail.com>
2023-01-25 23:46:13 +0530

    just started my java course

---
+++ Variable.java
@@ -0,0 +1,15 @@
+
+class Variable{
+	public static void main(String [] args){
+		int integer = 54;
+		boolean flag = true;
+		int binaryNumber = 0b10;
+		int octalNumber = 023;
+		int hexNumber = 0x2fde;
+		double myDoubleScientific = 3.445e5;
+		char initial = 'F';
+		String name = "Jayesh";
+		System.out.println(hexNumber);
+		System.out.println(name+myDoubleScientific);
+	}
+}
